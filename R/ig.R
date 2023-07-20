e95ccdfc61d48ff75ab42fbda7fc8c120ae0ad55
stla <laurent_step@yahoo.fr>
2023-07-20 11:45:24 +0200

    rnig

--- R/ig.R
+++ R/ig.R
@@ -9,3 +9,12 @@ rig <- function(n, mu, lambda) {
   z <- runif(n)
   ifelse(z < 1/(1+x), x, 1/x) * mu
 }
+
+#' @importFrom stats rnorm
+#' @noRd
+rnig <- function(n, alpha, beta, delta, mu) {
+  stopifnot(alpha > beta)
+  stopifnot(delta > 0)
+  z <- rig(n, delta, sqrt(alpha*alpha - beta*beta))
+  rnorm(n, mu + beta*z, sqrt(z))
+}
