fb64b63063b5259b55fc46881dc83a9653c32b52
stla <laurent_step@yahoo.fr>
2023-07-22 03:04:19 +0200

    png

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ ___
 
 ![](https://raw.githubusercontent.com/stla/boodist/main/inst/screenshots/hyperexponential.png)
 
+![](https://raw.githubusercontent.com/stla/boodist/main/inst/screenshots/inverseGaussian.png)
+
 ![](https://raw.githubusercontent.com/stla/boodist/main/inst/screenshots/normalInverseGaussian.png)
--- inst/essais/essai_inverseGaussian.R
+++ inst/essais/essai_inverseGaussian.R
@@ -17,14 +17,15 @@ axis(1L)
 sims <- ig$r(45000L)
 lines(density(sims))
 
-svg("x.svg")
+svg("x.svg", width = 8, height = 4)
+opar <- par(mar = c(3, 1, 3, 1))
 plot(
   x, y, type = "l", xlab = NA, ylab = NA, axes = FALSE,
-  lwd = 4, col = "blue", xaxs = "i", yaxs = "i", ylim = c(0, 0.8),
-  main = "Hyperexponential distribution"
+  lwd = 4, col = "blue", xaxs = "i", yaxs = "i", ylim = c(0, 0.76),
+  main = "Inverse Gaussian distribution"
 )
 axis(1L)
 dev.off()
 
-rsvg::rsvg_png("x.svg", "hyperexponential.png", width = 512, height = 512)
+rsvg::rsvg_png("x.svg", "inverseGaussian.png", width = 512, height = 256)
 
