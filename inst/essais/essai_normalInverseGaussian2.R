56847793e9aed1326f9738660949c5d7d21eec80
stla <laurent_step@yahoo.fr>
2023-07-24 16:00:59 +0200

    started qnig_rcpp

---
+++ inst/essais/essai_normalInverseGaussian2.R
@@ -0,0 +1,28 @@
+library(boodist)
+
+
+nig <- NormalInverseGaussian$new(0, 2, 1, 2)
+
+sims <- nig$r(55000L)
+
+plot(ecdf(sims))
+q_ <- seq(-2, 6, length.out = 100)
+y <- nig$p(q_)
+lines(q_, y, col = "red", lwd = 3, lty = "dashed")
+
+
+p <- 0.6
+atan_p <- atan(p)
+
+f <- function(aq) {
+  nig$p(tan(aq)) - 0.6
+}
+
+uroot <- uniroot(f, lower = -pi/2+0.01, upper = pi/2-0.01,
+                 tol = .Machine$double.eps^0.5)
+root <- uroot$root
+q <- tan(root)
+
+nig$p(q)
+
+
--- src/normalInverseGaussian.cpp
+++ src/normalInverseGaussian.cpp
@@ -1,8 +1,19 @@
-#include "boodist.h"
-#include <boost/math/special_functions/bessel.hpp>
 #include <RcppNumerical.h>
+#include <boost/math/special_functions/bessel.hpp>
+#include <boost/math/quadrature/gauss_kronrod.hpp>
+using namespace boost::math::quadrature;
+#include <boost/math/tools/roots.hpp>
+using namespace boost::math::tools;
+#include "boodist.h"
 using namespace Numer;
 #include <cmath>
+
+double dnig(double x, double mu, double alpha, double beta, double delta) {
+  double gamm = std::sqrt(alpha * alpha - beta * beta);
+  double s = std::sqrt(delta * delta + (x - mu) * (x - mu));
+  return alpha * delta * cyl_bessel_k(1, alpha * s) *
+         exp(delta * gamm + beta * (x - mu)) / (M_PI * s);
+}
 
 class NIGpdf : public Func {
  private:
@@ -25,10 +36,10 @@ class NIGpdf : public Func {
 
 // [[Rcpp::export]]
 Rcpp::NumericVector pnig_rcpp(Rcpp::NumericVector q,
-                     const double mu,
-                     const double alpha,
-                     const double beta,
-                     const double delta) {
+                              const double mu,
+                              const double alpha,
+                              const double beta,
+                              const double delta) {
   const double lower = -std::numeric_limits<double>::infinity();
   NIGpdf f(mu, alpha, beta, delta);
   const int subdiv = 150;
@@ -43,8 +54,8 @@ Rcpp::NumericVector pnig_rcpp(Rcpp::NumericVector q,
     int err_code;
     const double upper = q(i);
     const double res =
-      integrate(f, lower, upper, err_est, err_code, subdiv, eps_abs, eps_rel,
-                Integrator<double>::GaussKronrod201);
+        integrate(f, lower, upper, err_est, err_code, subdiv, eps_abs, eps_rel,
+                  Integrator<double>::GaussKronrod201);
     out(i) = res;
     error_estimate(i) = err_est;
     error_code(i) = err_code;
@@ -55,4 +66,41 @@ Rcpp::NumericVector pnig_rcpp(Rcpp::NumericVector q,
   out.attr("error_estimate") = error_estimate;
   out.attr("error_code") = error_code;
   return out;
+}
+
+// [[Rcpp::export]]
+Rcpp::NumericVector qnig_rcpp(double p,
+                              const double mu,
+                              const double alpha,
+                              const double beta,
+                              const double delta) {
+
+  auto pdf = [mu, alpha, beta, delta](double x) {
+    return dnig(x, mu, alpha, beta, delta)
+  };
+
+  const double lower = -std::numeric_limits<double>::infinity();
+
+  auto integral = [pdf, lower, p](double atanq) {
+    double error;
+    return gauss_kronrod<double, 61>::integrate(
+                  pdf, lower, std::tan(atanq), 0, 0, &error
+                ) - p;
+  };
+
+  const double a = -M_PI + 0.01;
+  const double b = M_PI - 0.01;
+  std::uintmax_t max_iter = 300;
+  std::pair<double, double> interval = toms748_solve(
+    integral,
+    a, b,
+    [](double l, double r){return abs(l-r) < 1e-6;},
+    max_iter
+  );
+  // if(err_code != 0) {
+  //   Rcpp::warning("An anomaly occured (see the error codes).");
+  // }
+  return Rcpp::NumericVector::create(
+    std::tan(interval.first), std::tan(interval.second)
+  );
 }
