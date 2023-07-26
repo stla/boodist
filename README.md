c206e8f6c966df0edc1c01556c7b42f8973b8db5
stla <laurent_step@yahoo.fr>
2023-07-26 10:23:54 +0200

    png gig

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ ___
 
 ![](https://raw.githubusercontent.com/stla/boodist/main/inst/screenshots/inverseGaussian.png)
 
+![](https://raw.githubusercontent.com/stla/boodist/main/inst/screenshots/generalizedInverseGaussian.png)
+
 ![](https://raw.githubusercontent.com/stla/boodist/main/inst/screenshots/normalInverseGaussian.png)
 
 ![](https://raw.githubusercontent.com/stla/boodist/main/inst/screenshots/skewNormal.png)
--- inst/essais/essai_generalizedInverseGaussian2.R
+++ inst/essais/essai_generalizedInverseGaussian2.R
@@ -1,6 +1,6 @@
 library(boodist)
 
-theta <- 1
+theta <- 2
 eta <- 2
 lambda <- 2
 
@@ -24,4 +24,33 @@ theta <- 2; eta <- 3; lambda <- 2
 q <- 3
 ( p <- gig$p(q) )
 gig$q(p)
+
+# picture ####
+theta <- 1
+eta <- 2
+lambda <- 2
+
+gig <- GeneralizedInverseGaussian$new(theta, eta, lambda)
+
+x <- seq(0, 25, length.out = 250L)
+y <- gig$d(x)
+
+opar <- par(mar = c(3, 1, 3, 1))
+plot(
+  x, y, type = "l", xlab = NA, ylab = NA, axes = FALSE,
+  lwd = 4, col = "blue", xaxs = "i", yaxs = "i", ylim = c(0, 0.1),
+  main = "Generalized inverse Gaussian distribution"
+)
+axis(1L)
+
+svg("x.svg", width = 8, height = 4)
+opar <- par(mar = c(3, 1, 3, 1))
+plot(
+  x, y, type = "l", xlab = NA, ylab = NA, axes = FALSE,
+  lwd = 4, col = "blue", xaxs = "i", yaxs = "i", ylim = c(0, 0.1),
+  main = "Generalized inverse Gaussian distribution"
+)
+axis(1L)
+dev.off()
+rsvg::rsvg_png("x.svg", "generalizedInverseGaussian.png", width = 512, height = 256)
 
