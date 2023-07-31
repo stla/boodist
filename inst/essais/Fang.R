baaa900cafb443f80ad8254b04a08f15c7736e1c
stla <laurent_step@yahoo.fr>
2023-07-31 14:07:35 +0200

    Fang continued

--- inst/essais/Fang.R
+++ inst/essais/Fang.R
@@ -9,9 +9,17 @@ names(Z) <- c("z1", "z2", "z3")
 dat <- cbind(dat, as.data.frame(Z))
 dat$u <- rep(1, N)
 groups <- split(dat, ~ Group)
+G <- 3L
+
+# prior parameters
+a0 <- rep(1, G)
+a1 <- matrix(1, nrow = G, ncol = d)
+a2 <- matrix(1, nrow = G, ncol = d)
+a3 <- rep(1, G)
+a4 <- rep(1, G)
+a5 <- array(1, dim = c(d, d, G))
 
 # initialization
-G <- 3L
 gamma <- delta <- rep(1, G)
 Mu <- aggregate(cbind(x1, x2) ~ Group, FUN = mean, data = dat)
 Mu <- cbind(Mu$x1, Mu$x2)
@@ -46,13 +54,14 @@ PrZ <- rho * vapply(1L:G, f, numeric(1L))
 Z <- rmultinom(N, 1L, PrZ)
 
 Levels <- c("setosa", "versicolor", "virginica")
-apply(Z, 2L, function(z) {
+grp <- apply(Z, 2L, function(z) {
   Levels[which(z == 1L)]
 })
-
+gindices <- lapply(Levels, function(lvl) {
+  which(grp == lvl)
+})
 
 #
-
 al <- function(g) {
   Sigma <- Delta[, , g]
   sqrt(gamma[g]^2 + t(Beta[g, ] %*% Sigma %*% Beta[g, ]))
@@ -69,5 +78,24 @@ newu <- function(i) {
   GeneralizedInverseGaussian$new(q*alpha, alpha/q, (d+1)/2)$r(1L)
 }
 
-vapply(1L:N, newu, numeric(1L))
+u <- vapply(1L:N, newu, numeric(1L))
 
+t0 <- rowSums(Z)
+t1 <- matrix(NA_real_, nrow = G, ncol = d)
+t2 <- matrix(NA_real_, nrow = G, ncol = d)
+t3 <- rep(NA_real_, G)
+t4 <- rep(NA_real_, G)
+t5 <- array(0, dim = c(d, d, G))
+
+y <- cbind(dat$x1, dat$x2)
+
+for(g in 1L:G) {
+  i <- gindices[[g]]
+  t1[g, ] <- colSums(y[i, , drop = FALSE])
+  t2[g, ] <- colSums(y[i, , drop = FALSE] / u[i])
+  t3[g] <- sum(u[i]) / 2
+  t4[g] <- sum(1/u[i]) / 2
+  for(j in i) {
+    t5[, , g] <- t5[, , g] + crossprod(y[j, , drop = FALSE]) / u[j]
+  }
+}
