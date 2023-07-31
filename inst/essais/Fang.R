41e47e126c7cb22fde8c3c25035e88afd248145e
stla <laurent_step@yahoo.fr>
2023-07-31 20:04:53 +0200

    Fang continued

--- inst/essais/Fang.R
+++ inst/essais/Fang.R
@@ -3,6 +3,20 @@ library(TruncatedNormal)
 library(mvtnorm)
 library(gtools)
 
+rMGIG <- function(q, z, A, b, d) {
+  p <- q + (1-d)/2
+  H <- tcrossprod(z)
+  theta <- sqrt(c(t(z) %*% A %*% z * b))
+  eta <- sqrt(b) / sqrt(c(t(z) %*% A %*% z))
+  gig <- GeneralizedInverseGaussian$new(
+    theta = theta, eta = eta, lambda = -p
+  )
+  x <- gig$r(1L)
+  W <- rWishart(1L, q, A)[, , 1L]
+  chol2inv(chol(x*H + W))
+}
+
+
 dat <- iris[, 3:5]
 d <- 2L
 N <- nrow(dat)
@@ -12,7 +26,8 @@ names(Z) <- c("z1", "z2", "z3")
 dat <- cbind(dat, as.data.frame(Z))
 dat$u <- rep(10, N)
 groups <- split(dat, ~ Group)
-G <- 3L
+Levels <- names(groups)
+G <- length(Levels)
 
 # prior parameters
 a0 <- rep(1, G)
@@ -34,11 +49,11 @@ Delta <- vapply(groups, function(df) {
   D <- det(Sigma)
   Sigma / D^(1/2)
 }, matrix(NA_real_, nrow = 2L, ncol = 2L))
+Lambda <- apply(Delta, 3, solve, simplify = FALSE)
 rho <- vapply(groups, nrow, integer(1L)) / nrow(dat)
 
 
-Lambda <- apply(Delta, 3, solve, simplify = FALSE)
-
+# Gibbs step 1
 f <- function(g) {
   df <- groups[[g]]
   n <- nrow(df)
@@ -54,11 +69,8 @@ f <- function(g) {
   }, numeric(1L))
   1/det(Sigma)^(n/2) * exp(sum(Z))
 }
-
 PrZ <- rho * vapply(1L:G, f, numeric(1L))
 Z <- rmultinom(N, 1L, PrZ)
-
-Levels <- c("setosa", "versicolor", "virginica")
 grp <- apply(Z, 2L, function(z) {
   Levels[which(z == 1L)]
 })
@@ -66,7 +78,7 @@ gindices <- lapply(Levels, function(lvl) {
   which(grp == lvl)
 })
 
-#
+# Gibbs step 2
 al <- function(g) {
   Sigma <- Delta[, , g]
   sqrt(gamma[g]^2 + t(Beta[g, ] %*% Sigma %*% Beta[g, ]))
@@ -82,7 +94,6 @@ newu <- function(i) {
   alph <- alpha[g]
   GeneralizedInverseGaussian$new(q*alph, alph/q, (d+1)/2)$r(1L)
 }
-
 u <- vapply(1L:N, newu, numeric(1L))
 
 t0 <- rowSums(Z)
@@ -92,9 +103,7 @@ t3 <- rep(NA_real_, G)
 t4 <- rep(NA_real_, G)
 t5 <- array(0, dim = c(d, d, G))
 S0 <- array(0, dim = c(d, d, G))
-
 y <- cbind(dat$x1, dat$x2)
-
 for(g in 1L:G) {
   i <- gindices[[g]]
   t1[g, ] <- colSums(y[i, , drop = FALSE])
@@ -107,14 +116,12 @@ for(g in 1L:G) {
   }
 }
 
-
 a0 <- a0 + t0
 a1 <- a1 + t1
 a2 <- a2 + t2
 a3 <- a3 + t3
 a4 <- a4 + t4
 a5 <- a5 + t5
-
 
 delta <- sqrt(rgamma(G, shape = a0/2+1, rate = a4 - a0^2/(4*a3)))
 gamma <- vapply(1:G, function(g) {
@@ -136,22 +143,7 @@ f <- function(g) {
 }
 MuBeta <- t(vapply(1L:G, f, numeric(2L*d)))
 
-
-rMGIG <- function(q, z, A, b, d) {
-  p <- q + (1-d)/2
-  H <- tcrossprod(z)
-  theta <- sqrt(c(t(z) %*% A %*% z * b))
-  eta <- sqrt(b) / sqrt(c(t(z) %*% A %*% z))
-  gig <- GeneralizedInverseGaussian$new(
-    theta = theta, eta = eta, lambda = -p
-  )
-  x <- gig$r(1L)
-  W <- rWishart(1L, q, A)[, , 1L]
-  chol2inv(chol(x*H + W))
-}
-
 Delta <- array(NA_real_, dim = c(d, d, G))
-
 for(g in 1L:G) {
   Delta[, , g] <- rMGIG(
     q = nu0+t0[g]/2, z = Beta[g, ], A = Lambda0 + S0[, , g], b = 2*t3[g], d = d
@@ -159,3 +151,6 @@ for(g in 1L:G) {
 }
 
 rho <- rdirichlet(1, a0)
+o <- order(rho)
+groups <- groups[o]
+Levels <- names(groups)
